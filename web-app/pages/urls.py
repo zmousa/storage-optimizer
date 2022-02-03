0f00338c982cb6d7f13b18fed634d6dbd357b6d6
Zaher Mousa <zaher.mousa@xing.com>
2022-02-03 19:50:25 +0100

    update home page checkboxes

--- web-app/pages/urls.py
+++ web-app/pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', lambda request: views.url_redirect(request), name="home"),
     path("drive", views.home, name="home"),
     path("drive/delete/<str:file_id>", views.delete, name="delete"),
     path("drive/imprint", views.imprint, name="imprint"),
--- web-app/pages/views.py
+++ web-app/pages/views.py
@@ -2,24 +2,60 @@ import service.drive_api as drive_api
 
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 
 
 def home(request):
-    google_client_id = request.COOKIES.get('google_client_id')
-    print(f"get cookie_client_id: {google_client_id}")
-    creds = drive_api.auth(google_client_id)
+    pdfs_duplicates = {}
+    images_duplicates = {}
+    videos_duplicates = {}
+    creds = None
+    switch_videos = False
+    switch_images = False
+    switch_pdfs = False
+    google_client_id = None
+    videos_grid = False
+    images_grid = False
+    pdfs_grid = False
 
-    pdfs = drive_api.list_files(creds, query=drive_api.get_mime_type("pdfs"))
-    images = drive_api.list_files(creds, query=drive_api.get_mime_type("images"))
-    videos = drive_api.list_files(creds, query=drive_api.get_mime_type("videos"))
+    if request.method == 'POST':
+        switch_videos = request.POST.get('switchCheckVideos', False)
+        switch_images = request.POST.get('switchCheckImages', False)
+        switch_pdfs = request.POST.get('switchCheckPdfs', False)
 
-    pdfs_duplicates = drive_api.get_duplicated_files_ids(pdfs)
-    images_duplicates = drive_api.get_duplicated_files_ids(images)
-    videos_duplicates = drive_api.get_duplicated_files_ids(videos)
-    response = render(request, 'home.html', {'pdfs': pdfs_duplicates, 'images': images_duplicates, 'videos': videos_duplicates})
-    if google_client_id is None:
-        print(f"set cookie_client_id: {creds.client_id}")
+        google_client_id = request.COOKIES.get('google_client_id')
+        creds = drive_api.auth(google_client_id)
+
+        if switch_pdfs:
+            pdfs = drive_api.list_files(creds, query=drive_api.get_mime_type("pdfs"))
+            pdfs_duplicates = drive_api.get_duplicated_files_ids(pdfs)
+        if switch_images:
+            images = drive_api.list_files(creds, query=drive_api.get_mime_type("images"))
+            images_duplicates = drive_api.get_duplicated_files_ids(images)
+        if switch_videos:
+            videos = drive_api.list_files(creds, query=drive_api.get_mime_type("videos"))
+            videos_duplicates = drive_api.get_duplicated_files_ids(videos)
+        videos_grid = switch_videos
+        images_grid = switch_images
+        pdfs_grid = switch_pdfs
+    else:
+        switch_videos = True
+        switch_images = True
+
+    response = render(request, 'home.html', {
+        'pdfs': pdfs_duplicates,
+        'images': images_duplicates,
+        'videos': videos_duplicates,
+        'videos_flag': 1 if switch_videos else 0,
+        'images_flag': 1 if switch_images else 0,
+        'pdfs_flag': 1 if switch_pdfs else 0,
+        'videos_grid': 1 if videos_grid else 0,
+        'images_grid': 1 if images_grid else 0,
+        'pdfs_grid': 1 if pdfs_grid else 0
+    })
+    if creds and google_client_id is None:
         response.set_cookie('google_client_id', creds.client_id)
+
     return response
 
 
@@ -33,3 +69,7 @@ def delete(request, file_id):
 
 def imprint(request):
     return render(request, 'imprint.html')
+
+
+def url_redirect(request):
+    return HttpResponseRedirect("/drive")
